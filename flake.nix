88b95cc4513596d36d51defef87fabe9bf7ccf60
SamueleFacenda <samuele.facenda@gmail.com>
2023-12-01 17:12:43 +0100

    Refactor repo
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
+# first line
 {
-  description = "An full-optional python flake template";
+  description = "Fantasy tabletennis!";
 
   # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
@@ -7,52 +8,43 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    let
-
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 lastModifiedDate;
-
-      overlay = final: prev: {
-
-      };
-    in
-
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = (nixpkgs.legacyPackages.${system}.extend overlay); in
+      let
+        pkgs = (nixpkgs.legacyPackages.${system}.extend (final: prev: {
+          # overlays
+        }));
+      in
       {
 
         packages = rec {
-            default = fitet-parser;
-            fitet-parser = pkgs.python3.pkgs.buildPythonApplication {
-              pname = "fitet-parser";
-              src = ./.;
-              inherit version;
-              pyproject = true;
-              #format = "pyproject";
+          default = fitet-parser;
+          fitet-parser = pkgs.python3.pkgs.buildPythonApplication {
+            pname = "fitet-parser";
+            src = ./.;
+            version = 0.1 .0;
+            pyproject = true;
+            #format = "pyproject";
 
-              propagatedBuildInputs = with pkgs.python3.pkgs; [
-                beautifulsoup4
-                requests
-                icecream
-                sqlalchemy
-              ];
+            propagatedBuildInputs = with pkgs.python3.pkgs; [
+              beautifulsoup4
+              requests
+              icecream
+              sqlalchemy
+            ];
 
-              nativeBuildInputs = with pkgs; [
-                python3.pkgs.setuptools
-              ];
+            nativeBuildInputs = with pkgs; [
+              python3.pkgs.setuptools
+            ];
 
-            };
           };
-          
+        };
+
         apps = {
-            default = {
-              type = "app";
-              program = "${self.packages.${system}.default}/bin/fitet-runner";
-            };
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/fitet-runner";
           };
+        };
 
         devShells = {
           default = pkgs.mkShell {
