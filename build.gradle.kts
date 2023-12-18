359e62289afad9795762126f63214002c9369f12
SamueleFacenda <samuele.facenda@gmail.com>
2023-12-18 14:50:01 +0100

    remove spring data rest

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     compileOnly("org.projectlombok:lombok")
