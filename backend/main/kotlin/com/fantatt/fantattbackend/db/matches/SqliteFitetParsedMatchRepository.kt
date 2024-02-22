f1ba2d20321e410d64d9363531e901641897b593
SamueleFacenda <samuele.facenda@gmail.com>
2024-02-22 11:30:57 +0100

    Finish the update procedure

--- backend/main/kotlin/com/fantatt/fantattbackend/db/matches/SqliteFitetParsedMatchRepository.kt
+++ backend/main/kotlin/com/fantatt/fantattbackend/db/matches/SqliteFitetParsedMatchRepository.kt
@@ -2,15 +2,34 @@ package com.fantatt.fantattbackend.db.matches
 
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
+import java.util.concurrent.Phaser
+import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.locks.ReentrantReadWriteLock
 
 @Component
 class SqliteFitetParsedMatchRepository: MatchRepository {
+    private val lock = ReentrantReadWriteLock()
+
     override fun findAllMatchesByPlayerBetweenDates(
         player: String,
         startDate: LocalDateTime,
         endDate: LocalDateTime
     ): List<RealMatch> {
-        TODO("Not yet implemented")
+        lock.readLock().lock()
+
+        try {
+            TODO()
+        } finally {
+            lock.readLock().unlock()
+        }
+    }
+
+    fun startUpdate() {
+        lock.writeLock().lock()
+    }
+
+    fun endUpdate() {
+        lock.writeLock().unlock()
     }
 
 }
--- backend/main/kotlin/com/fantatt/fantattbackend/lifecycle/FitetDbUpdateRunner.kt
+++ backend/main/kotlin/com/fantatt/fantattbackend/lifecycle/FitetDbUpdateRunner.kt
@@ -1,13 +1,29 @@
 package com.fantatt.fantattbackend.lifecycle
 
+import com.fantatt.fantattbackend.db.matches.SqliteFitetParsedMatchRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
+import java.io.File
 
 
-val UPDATE_CMD = "python -m fitet --update --dump-path ${SchedulingConfig().dumpPhat}"
-
+const val UPDATE_CMD = "python -m fitet --update --dump-path %s"
+@Autowired
+lateinit var sqliteMatchRepository: SqliteFitetParsedMatchRepository
+@Autowired
+lateinit var schedulingConfig: SchedulingConfig
 // one time per hour
-@Scheduled(cron = "0 0 * * * *")
+@Scheduled(cron = "0 2 * * * *")
 fun updateDb() {
-    // TODO some locking mechanism
-    Runtime.getRuntime().exec(UPDATE_CMD)
+    // update a copy of the database to block the access to the database for less
+    // time (just the time to copy the file)
+    val db = File(schedulingConfig.dumpPhat)
+    val tmpDb = File(schedulingConfig.dumpPhat + ".tmp")
+    db.copyTo(tmpDb, true)
+    Runtime.getRuntime().exec(UPDATE_CMD.format(tmpDb.path))
+
+    sqliteMatchRepository.startUpdate()
+    tmpDb.copyTo(db, true)
+    sqliteMatchRepository.endUpdate()
+
+    tmpDb.delete()
 }
